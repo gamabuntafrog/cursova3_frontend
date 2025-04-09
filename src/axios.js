97bca8785655334b33e6e107dd1bc5f6125baa18
gamabuntafrog <kirik.slaw@gmail.com>
2025-04-09 11:55:33 +0300

    update axios for using env key

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useAuthStore } from './stores/auth'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_BACKEND_URL,
 })
 
 instance.interceptors.request.use(
